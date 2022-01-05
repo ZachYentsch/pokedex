6153fa1dd03ecd2507b818ed141853f465a3bac0
ZachYentsch <z6chy@icloud.com>
2022-01-05 14:29:42 -0700

    All linked and Axios done

---
+++ app/Controllers/ApiPokeController.js
@@ -0,0 +1,7 @@
+
+
+export class ApiPokesController {
+    constructor() {
+
+    }
+}
---
+++ app/Services/MyPokesService.js
@@ -0,0 +1,5 @@
+import { ProxyState } from "../AppState.js";
+import { Pokemon } from "../Models/Pokemon.js";
+import { sboxApi } from "./AxiosService.js";
+
+
